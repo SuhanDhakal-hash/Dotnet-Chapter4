d7ee559c1c9873de39d1a9cd9ab94771532a76bc
SuhanDhakal-hash <suhandhakal65@gmail.com>
2026-05-26 08:24:51 +0545

    Delegate

---
+++ Chapter4/DelegateTypes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter4
+{
+    class MethodCollection
+    {
+        public static  void Printext()
+        {
+            Console.WriteLine("Action Delegate PrintMeassage");
+        }
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+     class DelegateTypes
+    {
+        static void Main(string[] args)
+        {
+            Action PrintTEXT = new Action(MethodCollection.Printext);
+            Func<int, int, int> add = new Func<int, int, int>(
+                MethodCollection.Addition);
+            PrintTEXT();
+            
+        }
+        
+    }
+}
---
+++ Chapter4/MulticastDelegate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Chapter4
+{
+    
+    delegate int NumberChanges(int x);
+
+    class MultiDelegate
+    {
+        static int num = 10;
+
+        
+        public static int AddNum(int x)
+        {
+            num += x;
+            return num;
+        }
+
+        
+        public static int MulNum(int x)
+        {
+            num *= x;
+            return num;
+        }
+
+        
+        public static int GetNum()
+        {
+            return num;
+        }
+    }
+
+    internal class MulticastDelegate
+    {
+        static void Main(string[] args)
+        {
+           
+            NumberChanges nc;
+
+            nc = new NumberChanges(MultiDelegate.AddNum);
+            nc += new NumberChanges(MultiDelegate.MulNum);
+
+            int result = nc(5);
+
+            Console.WriteLine("Result: " + result);
+            Console.WriteLine("Final Value of num: " + MultiDelegate.GetNum());
+
+            
+        }
+    }
+}
